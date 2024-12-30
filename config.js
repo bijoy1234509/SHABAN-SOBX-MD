b44d1be74ff0377b98c5401c78561f23fd80a456
bijoy1234509 <bijoymondal8509121704@gmail.com>
2024-12-30 18:19:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "SHABAN-SOBX-MD~CUwlhYoI#F1ywJ_UUCJYIgV839bkfmLOHsYPRgqrZutbKc7cDSF8",
+    SESSION_ID: process.env.SESSION_ID || "",
     CAPTION: process.env.CAPTION || "POWERED BY MRSHABAN432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
